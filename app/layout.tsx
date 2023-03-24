f7154c022ef37d4ec16e58fffa3b8f08e75d0015
Canaris <mrgeek484@gmail.com>
2023-03-24 21:44:18 +0800

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,7 @@ export default async function RootLayout({
 }) {
 	const session = await getServerSession(authOptions);
 
+
 	return (
 		<html lang='en' className={`${firaCode.variable} ${lato.variable} ${Cascadia_Code.variable} neon-green bg-black main-accent`}>
 			<head>
