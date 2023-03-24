63aef0d70d32bd6f49a142a9a6d697b3374d6dd1
Canaris <mrgeek484@gmail.com>
2023-03-24 21:54:29 +0800

    a

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,6 +5,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
+
 export default function App() {
 	return (
 		<div className='mt-36 w-2/5 m-auto'>
