725c0c65766364a31d5fe10cf67c2ff7c0ee2146
Canaris <mrgeek484@gmail.com>
2023-03-24 22:10:40 +0800

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Fira_Code, Lato } from 'next/font/google';
 import localFont from 'next/font/local';
 import type { Metadata } from 'next';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import SupabaseListener from '../components/AuthListener';
-import createClient from '../utils/supabase-server';
+// import SupabaseListener from '../components/AuthListener';
+// import createClient from '../utils/supabase-server';
 
 
 const Cascadia_Code = localFont({
@@ -46,11 +46,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-	const supabase = createClient();
+	// const supabase = createClient();
 
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
+	// const {
+	// 	data: { session },
+	// } = await supabase.auth.getSession();
 
 	return (
 		<html lang='en' className={`${firaCode.variable} ${lato.variable} ${Cascadia_Code.variable} neon-green bg-black main-accent`}>
@@ -58,7 +58,7 @@ export default async function RootLayout({
 				<meta name='google-site-verification' content='lmVMRfyduXED9G2uYI0Pb35uvXFj34LzRikL-wTm8WY' />
 			</head>
 			<body>
-				<SupabaseListener accessToken={session!?.access_token} />
+				{/* <SupabaseListener accessToken={session!?.access_token} /> */}
 				{/* top navbar */}
 				<div className='p-5 text-right font-fira bg-[rgba(0,0,0,0.8)] fixed top-0 w-full z-50'>
 					<Link href='/' className='mr-10 main-accent'>
@@ -73,17 +73,17 @@ export default async function RootLayout({
 					<Link href='/settings' className='mr-8 main-accent'>
 						<i className='fa-solid fa-gear'></i>
 					</Link>
-					{!session && <Link className='mr-4 main-accent' href='/auth'>
+					{/* {!session && <Link className='mr-4 main-accent' href='/auth'>
 						<i className='fa-solid fa-user'></i>
-					</Link>}
+					</Link>} */}
 					{/* display round pfp */}
-					{session && <Link href='/auth'>
+					{/* {session && <Link href='/auth'>
 						<img
 							className='rounded-full w-8 h-8 inline'
 							src={session.user.user_metadata!.avatar_url!}
 							alt='avatar'
 						/>
-					</Link>}
+					</Link>} */}
 				</div>
 				{children}
 			</body>
