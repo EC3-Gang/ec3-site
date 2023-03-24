e01d0e355a2af40f1ee45fff2a3f1f3bf4cf18d9
Canaris <mrgeek484@gmail.com>
2023-03-24 22:06:30 +0800

    a

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const supabase = createClient('https://pwjfsyerrwgtrzgrjnid.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3amZzeWVycndndHJ6Z3JqbmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk2MzMyMDYsImV4cCI6MTk5NTIwOTIwNn0.yU_UZgEMdFv2DRX4WxGAS0m0CU5MHpO_COX_Fznz3q0');
 
 
 export default function App() {
