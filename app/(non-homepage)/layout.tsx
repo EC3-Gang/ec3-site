47b2e8f1a32612c82a7b7c4f0619333d440e795b
Canaris <mrgeek484@gmail.com>
2023-04-15 14:41:02 +0800

    a

--- app/(non-homepage)/layout.tsx
+++ app/(non-homepage)/layout.tsx
@@ -3,17 +3,20 @@ import cp from 'child_process';
 import util from 'util';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import git from 'isomorphic-git';
+import fs from 'fs';
 dayjs.extend(relativeTime);
 const exec = util.promisify(cp.exec);
 
 export default async function NonHomepageLayout({ children }: { children: React.ReactNode[] | React.ReactNode }) {
-	// get both short and long commit hash and commit date
+	const dir = process.cwd();
+
 	try {
-		const [shortHash, hash, date] = (await Promise.all([
-			exec('git rev-parse --short HEAD'),
-			exec('git rev-parse HEAD'),
-			exec('git log -1 --format=%cd'),
-		])).map(str => str.stdout.trim());
+		const commitInfo = (await git.log({
+			fs,
+			dir,
+			depth: 1,
+		}))[0];
 
 
 		const year = new Date().getFullYear();
@@ -25,8 +28,10 @@ export default async function NonHomepageLayout({ children }: { children: React.
 					Made with ❤️, Next.js, and Tailwind CSS •&nbsp;
 					<a style={{
 						color: 'var(--text-color) !important',
-					}} className='hover:underline' target={'_blank'} href={`https://github.com/EC3-Gang/ec3-site/commit/${hash}`} rel='noreferrer'>{shortHash}</a> •&nbsp;
-					Built {dayjs(date).fromNow()}
+					}} className='hover:underline' target={'_blank'} href={`https://github.com/EC3-Gang/ec3-site/commit/${commitInfo.oid}`} rel='noreferrer'>
+						{commitInfo.oid.slice(0, 7)}
+					</a>&nbsp;
+					{/* • Built {dayjs().fromNow()} */}
 				</p>
 
 			</div>
